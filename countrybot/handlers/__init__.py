8b511355f98ec91369af84d4a9ea5ea174d2e18b
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-05-07 12:06:52 +0200

    Search By Name Callback Handler added

--- countrybot/handlers/__init__.py
+++ countrybot/handlers/__init__.py
@@ -1,7 +1,10 @@
 from telebot.async_telebot import AsyncTeleBot
 
 from .start import start
+from .search import search_by_name
 
 
 def register_handlers(bot: AsyncTeleBot):
     bot.register_message_handler(start, commands=['start'], pass_bot=True)
+
+    bot.register_callback_query_handler(search_by_name, lambda call: call.data == 'search_by_name', pass_bot=True)
