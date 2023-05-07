f265f9bf012cf4cd8b6230e8a310fa58227672c6
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-05-07 12:06:24 +0200

    States added

---
+++ countrybot/states.py
@@ -0,0 +1,12 @@
+from telebot.asyncio_handler_backends import State, StatesGroup
+
+
+class Search(StatesGroup):
+    search = State()
+    name = State()
+    code = State()
+    capital = State()
+    currency = State()
+    language = State()
+    calling_code = State()
+    selection = State()
